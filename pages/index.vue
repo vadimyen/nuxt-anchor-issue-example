8260070a91ae81eb12bab76158f6085ef8e5a51f
Вадим Еникеев <ve@smena.space>
2020-06-18 14:08:21 +0500

    anchor and link are inserted

--- pages/index.vue
+++ pages/index.vue
@@ -9,84 +9,78 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+      <nuxt-link :to="{path: $route.path, hash: '#anchor'}">ANCHOR</nuxt-link>
+      <p>Lorem ipsum eget auctor&nbsp;&mdash; elementum molestie et metus rutrum: at donec rutrum risus donec leo et
+        mattis arcu leo. Sagittis, quam vitae porta&nbsp;&mdash; commodo ultricies nec, tempus nec et enim. Orci
+        porttitor vitae vulputate pellentesque vivamus&nbsp;&mdash; rutrum&nbsp;&mdash; at sapien congue elementum porta
+        adipiscing odio diam ligula. Eros quam, bibendum arcu eros cursus orci <b>ut, magna</b> justo nulla diam in <b>massa.
+          Risus, cursus malesuada quisque</b> pharetra diam et pharetra integer amet tellus eu justo orci sit. Fusce
+        ultricies duis leo&nbsp;&mdash; ipsum, curabitur rutrum fusce, sit rutrum sed sagittis nibh&nbsp;&mdash; <i>elementum
+          congue adipiscing integer risus cursus sagittis</i> integer <b>sem lectus</b> ligula. At pellentesque nulla
+        tellus ligula porttitor duis bibendum risus ultricies ligula. At cursus urna mauris ipsum integer ultricies,
+        odio orci malesuada morbi sagittis sem.</p>
+      <p>Massa eu sem vivamus ipsum mattis quam nulla porttitor: cursus pharetra lorem: vitae quisque, commodo
+        pellentesque. Congue, bibendum: ultricies malesuada sed lectus, nibh in massa sem sit porta: in diam orci
+        adipiscing vitae amet nec. Donec arcu eu massa curabitur vivamus porttitor nam: sagittis bibendum donec odio,
+        curabitur lorem porta amet lorem ligula porttitor <i>sem, metus</i> magna ut&nbsp;&mdash; urna. Lorem adipiscing
+        sapien auctor ipsum sapien lorem curabitur ut ornare quam in metus maecenas eget diam. A ornare rutrum leo, quam
+        adipiscing odio orci magna mauris congue, ligula&nbsp;&mdash; sodales. Enim mattis amet donec massa ut at risus
+        nam: ultricies porta eget porttitor enim ut <b>mauris orci amet sem</b>&nbsp;&mdash; sodales porta, ornare
+        gravida.</p>
+      <p>Odio ornare donec justo nulla vitae ligula tellus non lectus sed proin, sit leo morbi, eget et&nbsp;&mdash;
+        lectus massa rutrum adipiscing nulla. Sagittis ligula curabitur maecenas non massa pharetra ipsum ornare
+        sagittis commodo vivamus, sem eros ut. Nec vitae, eros porta adipiscing congue pellentesque, pharetra leo
+        quisque porttitor <a href="#">nec integer auctor magna lectus sit</a> lorem donec commodo&nbsp;&mdash; <i>orci
+          odio duis morbi.</i></p>
+      <p>Metus lectus leo a nec sed molestie vivamus integer arcu vitae, adipiscing, urna nam bibendum porta quam eget
+        sagittis integer vitae molestie. Ornare lorem tellus, sit nam ligula proin sit metus ipsum mauris lectus
+        mauris.</p>
+      <p>Nec sem vitae, orci, gravida pellentesque donec: urna ultricies mauris ligula. Donec sapien ipsum non molestie
+        in pellentesque <b>eu arcu integer porta.</b> At risus orci vitae proin at cursus, bibendum elementum <a
+          href="#">a metus quam porta</a> eu molestie in eros. <i>Integer porttitor</i> commodo ligula odio vivamus sed
+        bibendum&nbsp;&mdash; ligula <i>molestie ut bibendum congue</i> molestie congue massa quam duis cursus massa
+        sagittis proin. Cursus nibh lorem arcu nulla duis ultricies sapien donec auctor magna mauris in commodo lectus,
+        nec morbi non auctor a et metus.</p>
+      <p>Elementum malesuada sem tempus pellentesque vulputate sit nec, magna&nbsp;&mdash; in eu donec sit bibendum in
+        <a href="#">commodo sem molestie eros orci, bibendum.</a> Integer, nec lectus congue, gravida ornare elementum
+        <a href="#">vitae&nbsp;&mdash; adipiscing: cursus nec</a> metus, proin sagittis, commodo congue, gravida integer
+        gravida et, cursus molestie curabitur, diam. Porttitor&nbsp;&mdash; ut eu magna, eros mauris sapien duis sodales
+        cursus: mattis ligula: integer metus nec auctor quisque quam urna sapien tempus.</p>
+      <p>Nibh rutrum metus, sit arcu proin pellentesque eros risus, lorem, congue donec malesuada proin magna quam nibh
+        cursus lorem massa eu porttitor auctor. Congue&nbsp;&mdash; sagittis massa proin at: sodales arcu integer
+        elementum magna amet in, mauris ornare vitae, tempus. Fusce vitae amet, eros ultricies elementum proin: urna
+        mauris, nec nam gravida pharetra.</p>
+      <p>Lectus odio morbi <b>duis justo malesuada</b> orci: morbi fusce&nbsp;&mdash; urna porttitor&nbsp;&mdash; odio
+        vivamus sodales. Metus, bibendum orci, lorem lectus ornare auctor integer proin ipsum mauris <i>pellentesque nam
+          eros</i> eu. Morbi&nbsp;&mdash; mauris proin, tellus magna nam eget vivamus integer gravida sodales mattis sed
+        odio in eros ornare. Eros commodo rutrum <a href="#">massa at porttitor&nbsp;&mdash; ligula</a> metus at nam
+        porttitor at non quam. Congue rutrum congue nec non morbi adipiscing fusce orci adipiscing <a href="#">ipsum
+          urna arcu lorem</a> maecenas integer sodales elementum. Justo magna <b>congue, sapien, mattis</b>, curabitur
+        morbi nibh cursus, curabitur nibh orci elementum magna, arcu commodo malesuada maecenas eu, sit. Metus tellus
+        quisque in bibendum, massa rutrum fusce <a href="#">nec amet, nam orci, porttitor odio eget</a> sagittis
+        elementum fusce. Mauris donec vivamus mauris <a href="#">orci morbi</a> urna quisque urna in massa ultricies
+        magna ut malesuada.</p>
+      <p>Quisque eu magna metus ipsum sed morbi porttitor eu congue: nulla eget enim a sodales odio proin odio leo: <a
+        href="#">tempus orci.</a> Pharetra duis pharetra, enim non: eros vivamus risus: eros nec quam duis metus
+        bibendum non metus <a href="#">elementum: vulputate</a> sed.</p>
+      <p id="anchor">Lorem lectus sagittis sed orci, risus ipsum porta in, quam ornare non diam arcu odio sit porttitor
+        vitae <i>gravida
+          et.</i> Morbi: bibendum: curabitur ut nam&nbsp;&mdash; ut, <a href="#">porttitor porta</a>: nulla sodales
+        pellentesque eget ornare ultricies eget sed ligula auctor amet <b>ligula non</b>, vitae <i>quisque: risus</i>
+        malesuada. Ornare eros, ornare <i>pellentesque arcu ipsum</i> vitae maecenas <i>urna eget</i> gravida eros, <i>sapien.</i>
+      </p>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+  import Logo from '~/components/Logo.vue'
+  import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  export default {
+    components: {
+      Logo,
+      VuetifyLogo
+    }
   }
-}
 </script>
